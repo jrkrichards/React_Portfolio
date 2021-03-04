b4bba4a70cca8dbb16f3f8fe8ebbdd42525adfab
Jayrich <jrkrichards@gmail.com>
2021-03-04 05:40:44 -0800

    trying a different router.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import createHistory from 'history/createBrowserHistory';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {createBrowserHistory} from 'history';
 import Home from "./pages/home";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import "./App.css";
 
 function App() {
-  const history = createHistory({
-    basename: process.env.PUBLIC_URL,
-  });
+  const history = createBrowserHistory();
   document.title = "JR_Portfolio";
   return (
     
-    <Router history={history}>
+    <Router history={history} basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
