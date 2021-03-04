688095995a7e8e5c8576f0d537ccc41bd3314727
Jayrich <jrkrichards@gmail.com>
2021-03-04 05:16:07 -0800

    added hashrouter for client rendering on ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
@@ -8,7 +8,7 @@ import "./App.css";
 function App() {
   document.title = "JR_Portfolio";
   return (
-    <Router>
+    <Router basename='/'>
       <Route exact path="/" component={Home} />
       <Route exact path="/projects" component={Projects} />
       <Route exact path="/contact" component={Contact} />
