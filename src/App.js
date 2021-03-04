6bc5f5a7d66396f2a31179424e0a06dc9e14581c
Jayrich <jrkrichards@gmail.com>
2021-03-04 05:33:47 -0800

    updated history for router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import createHistory from 'history/createBrowserHistory';
 import Home from "./pages/home";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import "./App.css";
 
 function App() {
+  const history = createHistory({
+    basename: process.env.PUBLIC_URL,
+  });
   document.title = "JR_Portfolio";
   return (
-    <Router basename='/'>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/contact" component={Contact} />
+    
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
     </Router>
+    
   );
 }
 
